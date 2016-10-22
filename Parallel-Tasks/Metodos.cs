47ce31a1a10010afe5a7a687cf4cb6fdc2d0832e
Allan Rojas <allanjose91@gmail.com>
2016-10-22 15:26:59 -0600

    Docume...

--- Parallel-Tasks/Metodos.cs
+++ Parallel-Tasks/Metodos.cs
@@ -9,16 +9,24 @@ namespace Parallel_Tasks
 {
     class Metodos
     {
+        /*
+         *· Determinar que cliente posee la mayor compra en un rango de 
+         * fechas determinado. (Este rango lo seleccionará el usuario de la aplicación)
+         * Recibe las fechas y la dirección del archivo a leer.
+         * Retorna un array de resultados de el cliente y el rendimiento.
+         */
 
         public ArrayList mayorCompra(string date1, string date2,string dirArchivoCompras)
         {
+            //Convierte las fechas a Datetime.
             DateTime Date1 = DateTime.Parse(date1);
             DateTime Date2 = DateTime.Parse(date2);
             //Start Timer 
             var watch = System.Diagnostics.Stopwatch.StartNew();
+            //Array de Resultados
             ArrayList cmc = new ArrayList();
+            // Variable comparación 
             string client= "No he encontrado cliente";
-            cmc.Add("Prueba en otras fechas" + "\n");
             
             try
             {
@@ -50,15 +58,21 @@ namespace Parallel_Tasks
                         {   //Obtenermos el monto de esa compra
                             //Esta en la posicion 4 y convertirlo a double.
                             double monto = Convert.ToDouble(values[5]);
+
+                            //Comparar el monto obtenido, con el mayor actual
                             if (monto >= montoMayor)
                             {
+                                //Si es mayor se actualiza el cliente 
+                                // y monto de la persona con mayor compra.
                                 montoMayor = monto;
                                 client = ("Este es el cliente con la mayor compra:\n"
                                     + values[1] + " Monto: " + values[5]  + " Fecha: " +
                                     values[6]);
                             }
                         }
+                        //Excepcion de formato
                         catch (FormatException) { cmc.Add("Format Exception\n"); }
+                        //Exception de Overflow
                         catch (OverflowException) { cmc.Add("OverflowException\n"); }
 
                     }
@@ -70,12 +84,18 @@ namespace Parallel_Tasks
                 cmc.Add("IO Exception | Busca un archivo de compras <-" + "\n");
 
             }
+            //Añade el cliente al array de respuesta
             cmc.Add(client);
+            //Detiene el reloj
             watch.Stop();
+            //Convertir de Milisegundos a Segundos
             var elapsedMs = watch.ElapsedMilliseconds;
             var sec = TimeSpan.FromMilliseconds(elapsedMs).TotalSeconds;
+            //Añade el tiempo al array de respuesta
             cmc.Add("Resultados Tareas:");
             cmc.Add("Tiempo Función (ms) : " + sec + "\n");
+
+            //Retorna el array de respuesta.
             return cmc;
         }
 
